0c872adb4a20d75214a1ff19de0b65cc02b88946
LucklessAura <paul.curea.andrei@gmail.com>
2020-04-07 16:38:06 +0300

    progress

---
+++ Host/Host/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using WCF;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Starting Server \n");
+            ServiceHost host = new ServiceHost(typeof(ClassesWCF), new Uri("http://localhost:8000/PC"));
+
+            foreach (ServiceEndpoint se in host.Description.Endpoints)
+            {
+                Console.WriteLine("adress : {0}\t binding : {1}\t contract : {2}", se.Address, se.Binding.Name, se.Contract.Name);
+            }
+
+            host.Open();
+            Console.WriteLine("Server Started");
+            Console.WriteLine("Press enter to close");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
